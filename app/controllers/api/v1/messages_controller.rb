a6e8d7644bd3bdc7aca53241f1badb690893f757
Ashnaali3255 <ashnaalipk@gmail.com>
2023-09-09 01:15:15 +0500

    Add linters

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::MessagesController < ApplicationController
   def random_greeting
-    random_message = Message.order("RANDOM()").first
-    render json: { greeting: random_message&.text } 
+    random_message = Message.order('RANDOM()').first
+    render json: { greeting: random_message&.text }
   end
 end
